624b070ebe8aa65e9aa4dc012c171a9d85e84b42
Tre Logan <tr3.logan@gmail.com>
2020-05-20 18:19:58 -0400

    Delete unused files

--- cmake-build-debug/fragment_shader.glsl
+++
@@ -1,7 +0,0 @@
-#version 430
-uniform sampler2D sampler;
-in vec2 fTexCoord;
-out vec4 color;
-void main(void) {
-    color = texture(sampler, fTexCoord.st);
-}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <glew.h>
 #include <glfw3.h>
 #include <iostream>
-#include "engine/Cube.h"
-#include "engine/Graphics.h"
-#include "engine/TextureLoader.h"
 
 const int WINDOW_WIDTH = 500;
 const int WINDOW_HEIGHT = 500;
@@ -19,39 +16,6 @@ int main() {
     glfwSwapInterval(1);
 
     if (glewInit() != GLEW_OK) std::cout << "Error initializing GLEW." << std::endl;
-
-    Graphics graphics(window, WINDOW_WIDTH, WINDOW_HEIGHT);
-    graphics.createShaderProgram("vertex_shader.glsl", "fragment_shader.glsl");
-
-    // Load textures
-    //FIXME Last loaded texture is used on all models.
-    // Setting the texture of a model doesn't affect it's texture.
-    //TODO Automatic texture unit allocation.
-    Texture *defaultTexture = TextureLoader::loadTexture("default_texture.jpg", GL_TEXTURE0);
-    Texture *brickTexture = TextureLoader::loadTexture("brick.png", GL_TEXTURE1);
-    std::cout << "Finished loading textures." << std::endl;
-
-    Cube *cube1 = new Cube(-1.0f, 0.0f, -5.0f);
-    Cube *cube2 = new Cube(1.0f, 0.0f, -5.0f);
-    cube2->setTexture(defaultTexture);
-    cube1->setTexture(brickTexture);
-
-    std::vector<Cube *> cubes;
-    cubes.push_back(cube1);
-    cubes.push_back(cube2);
-
-    while (!glfwWindowShouldClose(window)) {
-        auto it = cubes.begin();
-        while (it != cubes.end()) {
-            (*it)->setRotation((float) glfwGetTime());
-            it++;
-        }
-
-        graphics.draw(cubes);
-    }
-
-    delete(cube1);
-    delete(cube2);
 
     glfwTerminate();
 }
