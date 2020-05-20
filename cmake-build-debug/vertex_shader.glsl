37f78c377bb5efc7553ca2a8b54f66e3e28dd4b7
Tre Logan <tr3.logan@gmail.com>
2020-05-20 19:56:19 -0400

    Create animated triangle

--- cmake-build-debug/vertex_shader.glsl
+++ cmake-build-debug/vertex_shader.glsl
@@ -1,12 +1,7 @@
 #version 430
-in vec3 position;
-in vec2 vTexCoord;
-out vec2 fTexCoord;
-uniform mat4 view_matrix;
-uniform mat4 model_matrix;
-uniform mat4 rotation_matrix;
-uniform mat4 perspective_matrix;
+uniform float offset;
 void main(void) {
-    gl_Position = perspective_matrix * view_matrix * model_matrix * rotation_matrix * vec4(position, 1.0);
-    fTexCoord = vTexCoord;
+    if(gl_VertexID==0)gl_Position=vec4(0.25+offset,-0.25,0.0,1.0);
+    else if(gl_VertexID==1)gl_Position=vec4(-0.25+offset,-0.25,0.0,1.0);
+    else gl_Position=vec4(0.25+offset,0.25,0.0,1.0);
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,86 @@
 #include <glew.h>
 #include <glfw3.h>
 #include <iostream>
+#include "ShaderUtils.cpp"
 
-const int WINDOW_WIDTH = 500;
-const int WINDOW_HEIGHT = 500;
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+    std::string vShaderSource = ShaderReader::readShaderSource("vertex_shader.glsl");
+    std::string fShaderSource = ShaderReader::readShaderSource("fragment_shader.glsl");
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const char *vshader_str = vShaderSource.c_str();
+    const char *fshader_str = fShaderSource.c_str();
+
+    glShaderSource(vShader, 1, &vshader_str, NULL);
+    glShaderSource(fShader, 1, &fshader_str, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    ShaderErrorChecker::getCompileStatus(vShader);
+    ShaderErrorChecker::getCompileStatus(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    ShaderErrorChecker::checkOpenGLError();
+
+    return vfProgram;
+}
+
+void init(GLFWwindow *window) {
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
+
+float x = 0.0f; //location of triangle on x axis
+float inc = 0.01f; //offset for moving the triangle
+void display(GLFWwindow *window, double currentTime) {
+    glClear(GL_DEPTH_BUFFER_BIT);
+
+    //clear background to black each time
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(renderingProgram);
+    x += inc;
+    if (x > 1.0f)inc = -0.01f;
+    if (x < -1.0f)inc = 0.01f; //switch to moving the triangle to the right
+    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset"); //get ptr to "offset"
+    glProgramUniform1f(renderingProgram, offsetLoc, x); //send value in "x" to "offset"
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
 
 int main() {
 
-    if (!glfwInit()) std::cout << "Failed to initialize GLFW." << std::endl;
-
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
     glfwWindowHint(GLFW_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_VERSION_MINOR, 3);
-    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Window", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter2-program1", NULL, NULL);
     glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) {
+        exit(EXIT_FAILURE);
+    }
     glfwSwapInterval(1);
+    init(window);
 
-    if (glewInit() != GLEW_OK) std::cout << "Error initializing GLEW." << std::endl;
-
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
     glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
